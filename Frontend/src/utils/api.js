29f34b30d38fa8f9dda80c72dc777e1f1ac4491b
ADITYA PRATAP SINGH <aditya2602006@gmail.com>
2025-11-09 18:25:56 +0530

    Update api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",   // IMPORTANT: frontend will be served by backend, so use a relative path
+  baseURL: "https://novyn-electronics-e-commerce-website.onrender.com/api",   // IMPORTANT: frontend will be served by backend, so use a relative path
 });
 
 export default api;
