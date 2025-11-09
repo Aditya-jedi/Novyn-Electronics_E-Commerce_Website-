91c9cc3f70e0b6759be433054f8bda6241ce9ea3
ADITYA PRATAP SINGH <aditya2602006@gmail.com>
2025-11-09 14:34:46 +0530

    Update api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
+  baseURL: "https://novyn-electronics-e-commerce-website.onrender.com/api",
 });
 
 export default api;
