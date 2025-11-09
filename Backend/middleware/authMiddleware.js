a2659bb3094a1f24c0ae45d1a9e1fb043913a983
ADITYA PRATAP SINGH <aditya2602006@gmail.com>
2025-11-09 13:35:56 +0530

    Update authMiddleware.js

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 // middleware/authMiddleware.js
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const user = require("../models/user");
 // Protect routes (check if user is logged in)
 const protect = async (req, res, next) => {
   let token = req.headers.authorization;
